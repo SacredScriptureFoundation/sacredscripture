458c15f923ad87cf742223957ad0c5e5c5a118c2
Paul Benedict <pbenedict@sacredscripturefoundation.org>
2014-11-29 17:35:28 -0600

    Add javadoc and service attribute constraints

--- api/src/main/java/org/sacredscripture/platform/bible/service/AddChapterRequest.java
+++ api/src/main/java/org/sacredscripture/platform/bible/service/AddChapterRequest.java
@@ -1,21 +1,58 @@
+/*
+ * Copyright (c) 2014 Sacred Scripture Foundation.
+ * "All scripture is given by inspiration of God, and is profitable for
+ * doctrine, for reproof, for correction, for instruction in righteousness:
+ * That the man of God may be perfect, throughly furnished unto all good
+ * works." (2 Tim 3:16-17)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.sacredscripture.platform.bible.service;
+
+import org.sacredscripture.platform.bible.Chapter;
 
 import org.sacredscripturefoundation.commons.ServiceRequestMessage;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * This class is the request to create a new {@link Chapter} entity.
+ *
+ * @author Paul Benedict
+ * @see BibleMaintenanceService#add(AddChapterRequest)
+ * @since Sacred Scripture Platform 1.0
+ */
 public class AddChapterRequest extends ServiceRequestMessage {
 
     private String bibleCode;
     private String bookTypeCode;
     private String name;
 
+    @NotNull
+    @Size(max = 10)
     public final String getBibleCode() {
         return bibleCode;
     }
 
+    @NotNull
+    @Size(min = 3, max = 3)
     public final String getBookTypeCode() {
         return bookTypeCode;
     }
 
+    @NotNull
+    @Size(max = 10)
     public final String getName() {
         return name;
     }
--- api/src/main/java/org/sacredscripture/platform/bible/service/AddVerseRequest.java
+++ api/src/main/java/org/sacredscripture/platform/bible/service/AddVerseRequest.java
@@ -1,7 +1,38 @@
+/*
+ * Copyright (c) 2014 Sacred Scripture Foundation.
+ * "All scripture is given by inspiration of God, and is profitable for
+ * doctrine, for reproof, for correction, for instruction in righteousness:
+ * That the man of God may be perfect, throughly furnished unto all good
+ * works." (2 Tim 3:16-17)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.sacredscripture.platform.bible.service;
+
+import org.sacredscripture.platform.bible.Verse;
 
 import org.sacredscripturefoundation.commons.ServiceRequestMessage;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * This class is the request to create a new {@link Verse} entity.
+ *
+ * @author Paul Benedict
+ * @see BibleMaintenanceService#add(AddVerseRequest)
+ * @since Sacred Scripture Platform 1.0
+ */
 public class AddVerseRequest extends ServiceRequestMessage {
 
     private Long chapterId;
@@ -11,22 +42,30 @@ public class AddVerseRequest extends ServiceRequestMessage {
     private String text;
     private String code;
 
+    @NotNull
+    @Size(max = 10)
     public final String getAltName() {
         return altName;
     }
 
+    @NotNull
     public final Long getChapterId() {
         return chapterId;
     }
 
+    @Size(max = 10)
     public final String getCode() {
         return code;
     }
 
+    @NotNull
+    @Size(max = 10)
     public final String getName() {
         return name;
     }
 
+    @NotNull
+    @Size(max = 2000)
     public final String getText() {
         return text;
     }
