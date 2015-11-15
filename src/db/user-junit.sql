80125e7a4f6301b21bea8cd095dc07715a05c2d1
Paul Benedict <pbenedict@sacredscripturefoundation.org>
2015-11-14 22:52:34 -0600

    Point to unit test database

--- src/db/user-junit.sql
+++ src/db/user-junit.sql
@@ -1,4 +1,4 @@
--- Copyright (c) 2014 Sacred Scripture Foundation.
+-- Copyright (c) 2014, 2015 Sacred Scripture Foundation.
 -- "All scripture is given by inspiration of God, and is profitable for
 -- doctrine, for reproof, for correction, for instruction in righteousness:
 -- That the man of God may be perfect, throughly furnished unto all good
@@ -19,4 +19,4 @@
 CREATE USER 'junit'@'localhost' IDENTIFIED BY 'password1';
 
 GRANT SELECT, INSERT, UPDATE, DELETE 
-    ON sacredscripture.* TO 'junit'@'localhost';
+    ON sacredscripture_ut.* TO 'junit'@'localhost';
